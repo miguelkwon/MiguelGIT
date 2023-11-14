4ce94d43c31977cbc873b3e9aff52492a826f035
miguel.kwon <miguel.kwon@radiantvs.com>
2023-11-14 11:50:25 +0700

    s

--- PythonWorkspace/Search/search.py
+++ PythonWorkspace/Search/search.py
@@ -1,13 +1,39 @@
 from tkinter import *
-#import pyautogui
-import tkinter
+import pandas as pd
+from tkinter import filedialog
+
+
+
 
 
 
 search = Tk()
-
 search.title('Find parameter' )
 search.geometry("1200x300-500+140")
+
+
+
+
+
+def opseq():
+    sequence = filedialog.askopenfilename(
+        initialdir='G:/',
+        title = "sequence 파일을 선택 해 주세요",
+        filetypes= {("txt file","*.*")})
+       
+    
+
+
+
+ 
+
+
+
+btn1 = Button(search,text='OPEN sequence',bg='white',fg='black',command=opseq).grid(row=0,column=1)
+
+
+
+
 
 
 search.mainloop()
